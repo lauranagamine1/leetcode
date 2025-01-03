5ee0aef157ef49267e95eb46719bf15c6d29cbc4
Laura <laura.nagamine@utec.edu.pe>
2025-01-03 15:41:33 -0500

    merge strings problem

---
+++ 1-arrays,strings/1768-merge-strings.cpp
@@ -0,0 +1,45 @@
+//
+// Created by laura on 25/12/2024.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+string mergeAlternately(string word1, string word2) {
+    int n;
+    bool b;
+    if(word1.size()>word2.size()){
+        n = word1.size();
+        b = true;
+    } else{
+        n = word2.size();
+        b = false;
+    }
+
+    int contador=0;
+    string s;
+    if (b){ // word1 es mayor
+        for(int i=0; i<n; ++i){
+            s.push_back(word1[i]);
+            if(contador < word2.size() || contador == word2.size()){
+                s.push_back(word2[i]);
+                contador++;
+            }
+
+        }
+    } else{
+        for(int i=0; i<n; ++i){
+            if(contador < word1.size() || contador == word1.size()){
+                s.push_back(word1[i]);
+                contador++;
+            }
+            s.push_back(word2[i]);
+        }
+    }
+
+    return s;
+}
+
+int main() {
+    cout<<mergeAlternately("ab", "pqrs");
+    return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,9 @@
+//
+// Created by laura on 3/01/2025.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+}

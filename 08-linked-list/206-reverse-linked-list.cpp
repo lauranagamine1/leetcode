ea041ae16f2f7b46065dd546c24bb94286357c33
Laura <laura.nagamine@utec.edu.pe>
2026-02-28 13:31:17 -0500

    206 different approach

--- 08-linked-list/206-reverse-linked-list.cpp
+++ 08-linked-list/206-reverse-linked-list.cpp
@@ -11,8 +11,37 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+ListNode* reverse(ListNode* head, stack<int>& stack, ListNode* newHead) {
+    // newHead es lo mismo que head, pero apunta al primer nodo
+    if(stack.size()==0) return newHead; // caso base
+
+    ListNode* l = new ListNode(stack.top());
+    stack.pop();
+    head->next = l;
+
+    return reverse(head->next,stack,newHead);
+}
 
 ListNode* reverseList(ListNode* head) {
+    // base case
+    if(head == nullptr || head->next == nullptr) return head;
+    auto head2 = head;
+    // we'll use a stack
+    stack<int> stack;
+    int c=0;
+    while(head != nullptr){
+        stack.push(head->val);
+        c++;
+        head = head->next;
+    }
+    // tomamos el primer elemento
+    ListNode* l = new ListNode(stack.top());
+    stack.pop();
+
+    if(stack.size() >=1){
+        return reverse(l,stack,l);
+    }
+    return l;
 
 }
 
@@ -27,8 +56,12 @@ int main() {
     ListNode* l = new ListNode(1);
     ListNode* p = new ListNode(2);
     ListNode* t = new ListNode(3);
+    ListNode* u = new ListNode(4);
     l->next = p;
     l->next->next = t;
+    l->next->next->next = u;
+
+
     printList(l);
     cout<<endl;
     printList(reverseList(l));
